31c111d30bdabc9d1d523f07df923881e7216ba9
muhammadrizkyuniska <muhammadrizkykeno@gmail.com>
2023-06-11 22:40:16 +0800

    Melengkapi method matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -15,8 +15,20 @@ public class Matematika {
     }
     
 //    membuat method setPenjumlahan
-    double setPenjumlahan (){
+    public double setPenjumlahan (){
         return bil1 + bil2;
+    }
+    
+    public double setPengurangan(){
+        return bil1 - bil2;
+    }
+    
+    public double setPerkalian(){
+        return bil1 * bil2;
+    }
+    
+    public double setPembagian(){
+        return bil1 / bil2;
     }
     
 }
