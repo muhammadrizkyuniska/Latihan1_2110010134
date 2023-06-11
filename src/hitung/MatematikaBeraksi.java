0a5c8f5277f2eed5f551e8b23fe9cd0702be74c3
muhammadrizkyuniska <muhammadrizkykeno@gmail.com>
2023-06-11 22:54:09 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,14 @@
 package hitung;
 
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+//        membuat objeck
+        Matematika rizky = new Matematika(3,4);
+        
+        System.out.println("Hasil penjumlahan: "+rizky.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+rizky.setPengurangan());
+        System.out.println("Hasil perkalian: "+rizky.setPerkalian());
+        System.out.println("Hasil pembagian: "+rizky.setPembagian());
+    }
     
 }
