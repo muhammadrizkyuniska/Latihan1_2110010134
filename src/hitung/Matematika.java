06a1cc2f9a26aefe8806fe13867fa8a131257030
muhammadrizkyuniska <muhammadrizkykeno@gmail.com>
2023-06-11 22:32:22 +0800

    Membuat contructor dan setPenjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,5 +1,22 @@
 package hitung;
 
 public class Matematika {
+//    membuat variabel 
+    double bil1,bil2;
+
+//    membuat Constructor pakai Parameter
+    public Matematika(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+
+//    membuat Constructor tanpa Parameter
+    public Matematika() {
+    }
+    
+//    membuat method setPenjumlahan
+    double setPenjumlahan (){
+        return bil1 + bil2;
+    }
     
 }
